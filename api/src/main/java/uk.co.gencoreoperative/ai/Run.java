9f6e9c5cb1e886f409dbe49c90a8bae76538571c
Robert Wapshott <robert@wapshott.org.uk>
2024-12-26 11:54:53 +0000

    Adding the ability to use system and user prompts to the API

--- api/src/main/java/uk.co.gencoreoperative/ai/Run.java
+++ api/src/main/java/uk.co.gencoreoperative/ai/Run.java
@@ -9,7 +9,18 @@ public interface Run {
     /**
      * Perform a single AI request with the provided prompt.
      * @param prompt A non-null, possibly empty prompt.
+     * @return A possibly empty response from the LLM.
      * @throws RuntimeException If there was an error performing the request.
      */
     String run(@Nonnull String prompt) throws RuntimeException;
+
+    /**
+     * Perform a single AI request with the provided system and user prompts.
+     * @param system A non-null text that guides the intention of the LLM request.
+     * @param user The text provided from the user that the LLM is to respond to.
+     * @return A possibly empty response from the LLM.
+     * @throws RuntimeException If there was an error performing the request.
+     */
+    String run(@Nonnull String system, @Nonnull String user) throws RuntimeException;
+
 }
--- api/src/main/java/uk.co.gencoreoperative/llama3/Llama3Runner.java
+++ api/src/main/java/uk.co.gencoreoperative/llama3/Llama3Runner.java
@@ -17,6 +17,11 @@ import mukel.Llama3;
  * <p>
  * The code provided was intended for use on the command line. This class adapts this by
  * capturing the output of the model and returning it as a string.
+ * <p>
+ * The Llama3.java library we are using is best invoked using its {@link mukel.Llama3#main(String[])}
+ * method. This call will generate stdout and stderr that will be re-diverted to simplify the
+ * experience for the caller. From the caller's perspective, we are only interested in the output
+ * of the model.
  */
 public class Llama3Runner implements Run {
     private final Path modelPath;
@@ -27,22 +32,41 @@ public class Llama3Runner implements Run {
 
     /**
      * Perform a single AI request with the provided prompt.
-     * <p>
-     * The Llama3.java library we are using is best invoked using its {@link mukel.Llama3#main(String[])}
-     * method. This call will generate stdout and stderr that will be re-diverted to simplify the
-     * experience for the caller.
-     * <p>
-     * From the caller's perspective, we are only interested in the output of the model.
      *
      * @param prompt A non-null, possibly empty prompt.
      * @return The output of the model if any.
      */
     @Override
     public String run(@Nonnull String prompt) {
-        String[] args = {"--model", modelPath.toString(), "--prompt", prompt, "--instruct"};
+        String[] args = {
+                "--model", modelPath.toString(),
+                "--instruct",
+                "--prompt", prompt
+                };
+        return llama3(args);
+    }
+
+    /**
+     * Perform a single AI request with the provided system and user prompts.
+     *
+     * @param system A non-null text that guides the intention of the LLM request.
+     * @param user The text provided from the user that the LLM is to respond to.
+     * @return The output of the model if any.
+     */
+    public String run(@Nonnull String system, @Nonnull String user) {
+        String[] args = {
+                "--model", modelPath.toString(),
+                "--instruct",
+                "--system-prompt", system,
+                "--prompt", user
+        };
+        return llama3(args);
+    }
+
+    private static String llama3(String[] args) {
         return StdOutUtils.executeWithRedirect(() -> {
             try {
-                mukel.Llama3.main(args);
+                Llama3.main(args);
             } catch (IOException e) {
                 throw new RuntimeException(format("Failed to run Llama3: {0}", e.getMessage()), e);
             }
