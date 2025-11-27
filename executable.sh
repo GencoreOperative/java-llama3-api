dfe766dfeaa5f9b37aae11c4e2a82779312f2d47
Robert Wapshott <robert@wapshott.org.uk>
2025-11-27 15:16:29 +0000

    Updating the executable Jar concept to include the incubator functionality for Llama3.

--- executable.sh
+++ executable.sh
@@ -3,7 +3,9 @@
 
 # $1 = path to input jar
 # $2 = path to output binary
-# Example usage: ./executable.sh target/mp3tag-1.0-SNAPSHOT.jar mp3tag
+# Example usage: ./executable.sh target/project-1.0-SNAPSHOT.jar project
+
+# This script has been customised for this particular project with jdk features enabled.
 
 INPUT_JAR="$1"
 OUTPUT_BIN="$2"
@@ -11,7 +13,7 @@ OUTPUT_BIN="$2"
 # Prepend launcher to the jar
 (cat <<'SH'
 #!/usr/bin/env sh
-exec java -jar "$0" "$@"
+exec java --enable-preview --add-modules=jdk.incubator.vector -Djdk.incubator.vector.VECTOR_ACCESS_OOB_CHECK=0 -jar "$0" "$@"
 exit 0
 SH
 cat "$INPUT_JAR") > "$OUTPUT_BIN"
